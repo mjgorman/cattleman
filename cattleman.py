c79c546c5c1d21545cc85325972244eacb7c26f4
Michael Gorman <mgorman@tuition.io>
2016-09-22 14:14:42 -0700

    Add requirements.txt, structure Dockerfile for quicker rebuilds on app changes, add checks for required environment values

--- cattleman.py
+++ cattleman.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -9,6 +10,9 @@ class cattleman(object):
         self.api_user = os.getenv('RANCHER_USER')
         self.api_key = os.getenv('RANCHER_KEY')
         self.api_url = os.getenv('RANCHER_URL')
+        if not self.api_user or not self.api_key or not self.api_url:
+            print("Please set RANCHER_USER, RANCHER_KEY and RANCHER_URL Environment Variables")
+            sys.exit(1)
 
     def test_connection(self):
         print("Connecting to the Rancher API...")
